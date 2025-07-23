6e45d1d0571b23f109acbbbe965ccadb410b9d7e
jangwoojoo <jwscape01@gmail.com>
2025-07-23 14:53:35 +0900

    Delete src/jungol directory

--- src/jungol/출력_자가진단4.java
+++
@@ -1,9 +0,0 @@
-package jungol;
-
-public class 출력_자가진단4 {
-    public static void main(String[] args) {
-        System.out.println("(@) (@)");
-        System.out.println("(=^.^=)");
-        System.out.println("(-m-m-)");
-    }
-}
