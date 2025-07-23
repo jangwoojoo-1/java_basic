b718abe4411538674f10f609ddd7e764c3fd579b
jangwoojoo-1 <jwscape01@gmail.com>
2025-07-23 17:39:33 +0900

    jungol_practice_second_commit "src/jungol/\354\266\234\353\240\245_\354\227\260\354\212\265\353\254\270\354\240\2343.java" "src/jungol/\354\266\234\353\240\245_\354\227\260\354\212\265\353\254\270\354\240\2345.java"

--- src/jungol/출력_연습문제3.java
+++ src/jungol/출력_연습문제3.java
@@ -2,9 +2,9 @@ package jungol;
 
 public class 출력_연습문제3 {
     public static void main(String[] args) {
-        System.out.println("세 번째 프로그램입니다.");
-        System.out.println("줄을 바꾸어 출력합니다.");
-        System.out.println("여기까지 출력하고");
-        System.out.println("줄을 바꿉니다.");
+        System.out.print("세 번째 프로그램입니다. \n" +
+                "줄을 바꾸어 출력합니다. \n" +
+                "여기까지 출력하고 \n" +
+                "줄을 바꿉니다.");
     }
 }
---
+++ src/jungol/출력_연습문제8.java
@@ -0,0 +1,13 @@
+package jungol;
+
+public class 출력_연습문제8 {
+    public static void main(String[] args) {
+        System.out.println(" subject score");
+        for(int i=0;i<=13;i++){
+            System.out.print("=");
+        }
+        System.out.print("\n  korean    90\n " +
+                "english   100\n" +
+                "computer    80");
+    }
+}
