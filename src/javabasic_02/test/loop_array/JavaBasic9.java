368cd48087cae51b3bbaf05013e83f96c63e7a3e
jangwoojoo-1 <jwscape01@gmail.com>
2025-07-31 23:35:17 +0900

    day8_practice

--- src/javabasic_02/test/loop_array/JavaBasic9.java
+++ src/javabasic_02/test/loop_array/JavaBasic9.java
@@ -8,66 +8,131 @@ public class JavaBasic9 {
 
     public static void main(String[] args) {
         boolean tf = true;
-        Integer[] scores = null;
-        int count = 0;
+        int[] scores = null;
 
         while(tf){
-            System.out.println("--------------------------------------------------");
-            System.out.println("1.학생수 | 2.점수입력 | 3.점수리스트 | 4.분석 | 5.종료");
-            System.out.println("--------------------------------------------------");
-            System.out.print("선택> ");
-            String input = sc.nextLine();
+            printMenu();//메뉴판 출력
 
-            if (input.length() != 1 || input.charAt(0) < '1' || input.charAt(0) > '5') {
-                System.out.println("1~5 중 하나의 숫자로 입력해주세요.");
-                continue;
-            }
-
-            char n = input.charAt(0);
+            char n = selectMenu(); //번호 선택
 
             switch (n){
                 case '1':
-                    System.out.print("학생수> ");
-                    count = Integer.parseInt(sc.nextLine());
-                    scores = new Integer[count];
+                    scores = inputStudentCount();
                     break;
                 case '2':
-                    for (int i = 0 ; i < count ; i++){
-                        System.out.printf("scores[%d]> ", i);
-                        int num = Integer.parseInt(sc.nextLine());
-                        while(num < 0 || num > 100){
-                            System.out.printf("scores[%d]> ", i);
-                            num = Integer.parseInt(sc.nextLine());
-                        }
-                        scores[i] = num;
-                    }
+                    inputStudentScore(scores);
                     break;
                 case '3':
-                    for (int i = 0; i < count; i++) {
-                        System.out.printf("scores[%d]: %d\n", i, scores[i]);
-                    }
+                    printStudentScore(scores);
                     break;
                 case '4':
-                    int max = 0;
-                    for(int i = 0 ; i < count ; i++){
-                        if(max < scores[i]){
-                            max = scores[i];
-                        }
-                    }
-                    System.out.println("최고 점수: " + max);
-                    int total = 0;
-                    for(int i = 0 ; i < count ; i++){
-                        total += scores[i];
-                    }
-                    System.out.printf("평균 점수: %.1f\n", (double)total/count);
+                    analyzeScore(scores);
                     break;
                 case '5':
-                    System.out.println("프로그램 종료");
+                    programExit();
                     tf = false;
                     break;
                 default:
                     System.out.println("숫자를 잘못 입력했습니다.");
             }
         }
+    }
+
+    public static void printMenu(){
+        System.out.println("--------------------------------------------------");
+        System.out.println("1.학생수 | 2.점수입력 | 3.점수리스트 | 4.분석 | 5.종료");
+        System.out.println("--------------------------------------------------");
+        System.out.print("선택> ");
+    }
+
+    public static char selectMenu(){
+        String input = sc.nextLine();
+
+        while (input.length() != 1 || input.charAt(0) < '1' || input.charAt(0) > '5') {
+            System.out.println("1~5 중 하나의 숫자로 입력해주세요.");
+            input = sc.nextLine();
+        }
+
+        return input.charAt(0);
+    }
+
+    public static int[] inputStudentCount(){
+
+        while (true) {
+            System.out.print("학생수> ");
+            try {
+                int count = Integer.parseInt(sc.nextLine());
+                if (count > 0) {
+                    int[] array = new int[count];
+                    array[0] = -1;//후에 입력 확인을 위해서
+                    return array;
+                } else {
+                    System.out.println("학생 수를 다시 입력하세요.");
+                }
+            } catch (NumberFormatException e) {//숫자가 아닌 입력 예외처리
+                System.out.println("정수를 입력해주세요.");
+            }
+        }
+    }
+
+    public static boolean arrayIsNull(int[] array){
+        return array == null;
+    }
+
+    public static void inputStudentScore(int[] array){
+        if(arrayIsNull(array)){
+            System.out.println("학생수부터 입력해주시기 바랍니다.");
+            return;
+        }
+        for (int i = 0 ; i < array.length ; i++){
+            while (true) {
+                System.out.printf("scores[%d]> ", i);
+                try {
+                    int score = Integer.parseInt(sc.nextLine());
+                    if (score < 0 || score > 100) {
+                        System.out.println("0~100 사이의 점수를 입력해주세요.");
+                    } else {
+                        array[i] = score;
+                        break;
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("숫자로 입력해주세요.");
+                }
+            }
+        }
+    }
+    
+    public static void printStudentScore(int[] array){
+        if(arrayIsNull(array)){
+            System.out.println("학생수부터 입력해주시기 바랍니다.");
+            return;
+        }
+        if (array[0] == -1) {
+            System.out.println("점수 입력을 부탁드립니다.");
+            return;
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.printf("scores[%d]: %d\n", i, array[i]);
+        }
+    }
+
+    public static void analyzeScore(int[] array){
+        if(arrayIsNull(array)){
+            System.out.println("학생수부터 입력해주시기 바랍니다.");
+            return;
+        } else if (array[0] == -1) {
+            System.out.println("점수 입력을 부탁드립니다.");
+            return;
+        } else {
+            int maxScore = Arrays.stream(array).max().orElse(0);
+            int total = Arrays.stream(array).sum();
+            System.out.println("최고 점수: " + maxScore);
+            System.out.printf("평균 점수: %.1f\n", (double)total/array.length);
+        }
+    }
+
+    public static void programExit(){
+        System.out.println("프로그램 종료");
     }
 }
