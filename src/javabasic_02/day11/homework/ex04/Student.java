3d6f3ed94f14adafe996f6a09a560868c4242e9e
jangwoojoo-1 <jwscape01@gmail.com>
2025-08-06 17:31:32 +0900

    day12 3rd commit

--- src/javabasic_02/day11/homework/ex04/Student.java
+++ src/javabasic_02/day11/homework/ex04/Student.java
@@ -28,9 +28,22 @@ public class Student {
 				this.name, this.subject, this.fee, this.returnFee);
 	}
 
-	public static void main(String args[]){
-		Student stu = new Student("장동건", "jspprogram", 500000);
+	public static void main(String[] args){
+		if (args.length != 3) {
+			System.out.println("사용법: java Student 이름 과정명 교육비");
+			return;
+		}
+
+		String name = args[0];
+		String subject = args[1];
+		int fee = Integer.parseInt(args[2]);
+
+		Student stu = new Student(name, subject, fee);
 		stu.calcReturnFee();
 		stu.print();
 	}
+
+	// 마지막에 문제에서 주어진 main 메서드처럼 args로 이름, 과정명, 교육비 데이터를 받아서 Student 클래스 객체 생성해서 작성
+	// 이게 무슨 말인지 제대로 이해 못해서 copilot 참고해서 IDE에서 실행설정에 인자를 넣어
+	//args를 통해 실행이 되도록 했습니다.
 }
